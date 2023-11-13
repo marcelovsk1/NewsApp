759cedb8c4d6a1c184e7183f27bc9e959db4d967
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 15:39:26 -0300

    implementing kingfisher

--- NewsApp/Utilities/NewsCardComponent.swift
+++ NewsApp/Utilities/NewsCardComponent.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct NewsCardComponent: View {
+    var article: NewsResponse.Article
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomLeading) {
+            
+        }
+        .padding(4)
+        .frame(maxWidth: .infinity, maxHeight: 230, alignment: .leading)
+        .cornerRadius(8)
+        .shadow(radius: 4)
     }
 }
 
 #Preview {
-    NewsCardComponent()
+    NewsCardComponent(article: NewsResponse.Article(title: "", url: ""))
 }
