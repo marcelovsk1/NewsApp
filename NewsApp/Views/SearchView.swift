1ccb5cb228f1fcd34542eb683f802cfb5a7d3bbf
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 16:13:02 -0300

    working on searchview

--- NewsApp/Views/SearchView.swift
+++ NewsApp/Views/SearchView.swift
@@ -16,7 +16,56 @@ struct SearchView: View {
     
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Search...", text: $searchText)
+                        .padding(.leading, 20)
+                    
+                    if searchText.isEmpty {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .padding(.leading, 8)
+                    } else {
+                        Button {
+                            searchText = ""
+                            updateNewsArticles()
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                        }
+                    }
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal, 16)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+            }
+            .preferredColorScheme(.dark)
+            .onAppear(perform: updateNewsArticles)
+            .onChange(of: selectedGenre) { _ in
+                isLoading = true
+                updateNewsArticles()
+            
+            }
+            .onChange(of: searchText) { _ in
+                isLoading = true
+                updateNewsArticles()
+            }
+        }
+    }
+    
+    private func updateNewsArticles() {
+        newsManager.getNews(category: selectedGenre) { newsResponse in
+            articles = newsResponse
+            isLoading = false
+        }
+    }
+    
+    private func updateNewsArticlesFromSearch() {
+        newsManager.getNews(category: searchText) { newsResponse in
+            articles = newsResponse
+            isLoading = false
+        }
     }
 }
 
