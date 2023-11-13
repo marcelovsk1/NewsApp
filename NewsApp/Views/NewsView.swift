946a1a040620ed36aa5ed31f591d6edcdbc25c47
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 15:49:07 -0300

    start to work on newsview

--- NewsApp/Views/NewsView.swift
+++ NewsApp/Views/NewsView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct NewsView: View {
+    var newsManager = NewsManager()
+    @State private var articles = [NewsResponse.Article]()
+    @State private var isLoading = true
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
