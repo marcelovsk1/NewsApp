3ae210d9355efaa1d2fb07e72dfa80d198f1e26f
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 14:32:44 -0300

    organizing the apps folders

---
+++ NewsApp/Views/NewsView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsView.swift
+//  NewsApp
+//
+//  Created by Marcelo Amaral Alves on 2023-11-13.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsView()
+}
---
+++ NewsApp/Views/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  NewsApp
+//
+//  Created by Marcelo Amaral Alves on 2023-11-13.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
