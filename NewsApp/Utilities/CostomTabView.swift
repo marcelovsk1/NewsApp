9e5cbff1aeda35144e3a35c2def0c5b82f974353
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 14:49:32 -0300

    updating costomtabview

--- NewsApp/Utilities/CostomTabView.swift
+++ NewsApp/Utilities/CostomTabView.swift
@@ -14,7 +14,46 @@ struct CostomTabView: View {
     let tabBaritems: [(image: String, title: String)] = [("newspaper.fill", "News"), ("magnifyingglass", "Search"), ("gear", "Settings")]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Capsule()
+                .frame(height: 80)
+                .foregroundColor(Color(.secondarySystemBackground))
+                .shadow(radius: 2)
+            
+            HStack {
+                ForEach(0..<3) { index in
+                    Button {
+                        tabSelection = index + 1
+                    } label: {
+                        VStack(spacing: 8) {
+                            Spacer()
+                            
+                            Image(systemName: tabBaritems[index].image)
+                            
+                            Text(tabBaritems[index].title)
+                                .font(.caption)
+                            
+                            if index + 1 == tabSelection {
+                                Capsule()
+                                    .frame(height: 8)
+                                    .foregroundColor(.indigo)
+                                    .matchedGeometryEffect(id: "SelectedTabID", in: AnimationNameSpace)
+                                    .offset(y: 3)
+                            } else {
+                                Capsule()
+                                    .frame(height: 8)
+                                    .foregroundColor(.clear)
+                                    .offset(y: 3)
+                            }
+                        }
+                        .foregroundColor(index + 1 == tabSelection ? .indigo : .gray)
+                    }
+                }
+            }
+            .frame(height: 80)
+            .clipShape(Capsule())
+        }
+        .padding(.horizontal)
     }
 }
 
