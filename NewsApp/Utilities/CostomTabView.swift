e1f1b5fb39540cb04db7dc9645cc43f78961e7bf
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 14:40:19 -0300

    adding tabaritems

--- NewsApp/Utilities/CostomTabView.swift
+++ NewsApp/Utilities/CostomTabView.swift
@@ -11,6 +11,8 @@ struct CostomTabView: View {
     @Binding var tabSelection: Int
     @Namespace private var AnimationNameSpace
     
+    let tabBaritems: [(image: String, title: String)] = [("newspaper.fill", "News"), ("magnifyingglass", "Search"), ("gear", "Settings")]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
