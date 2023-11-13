7b8d420e5f117f896f93fef5f3e6023206ba0c3a
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 16:01:12 -0300

    updating the api url

--- NewsApp/Models/NewsManager.swift
+++ NewsApp/Models/NewsManager.swift
@@ -10,9 +10,32 @@ import Foundation
 class NewsManager {
     private let apiKey = "b055858163ee4232bda92ee54488e44b"
     
-    func getNew(category: String, completion: @escaping ([NewsResponse.Article]) -> Void) {
+    func getNews(category: String, completion: @escaping ([NewsResponse.Article]) -> Void) {
         guard let url = URL(string:
             "https://newsapi.org/v2/top-headlines?country=us&category=\(category)&apiKey=\(apiKey)") 
+        else {
+            print("Failure to make URL")
+            return
+        }
+                
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Failure to get data")
+                return
+            }
+            
+            do {
+                let newResponse = try JSONDecoder().decode(NewsResponse.self, from: data)
+                completion(newResponse.articles)
+            } catch {
+                print("Failure to convert data \(error.localizedDescription)")
+            }
+        }.resume()
+    }
+    
+    func getNewsFromSearch(search: String, completion: @escaping ([NewsResponse.Article]) -> Void) {
+        guard let url = URL(string:
+            "https://newsapi.org/v2/everything?q=\(search)&apiKey=\(apiKey)")
         else {
             print("Failure to make URL")
             return
