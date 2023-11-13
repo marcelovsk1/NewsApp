ae5e4c72ad68b12b9d973bc370543095c5917e61
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 15:36:25 -0300

    creating newscardcomponent

---
+++ NewsApp/Utilities/NewsCardComponent.swift
@@ -0,0 +1,18 @@
+//
+//  NewsCardComponent.swift
+//  NewsApp
+//
+//  Created by Marcelo Amaral Alves on 2023-11-13.
+//
+
+import SwiftUI
+
+struct NewsCardComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsCardComponent()
+}
