7bbbf46555e2c04b36cf000726ba821e8957a50b
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-13 15:45:59 -0300

    working on

--- NewsApp/Utilities/NewsCardComponent.swift
+++ NewsApp/Utilities/NewsCardComponent.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct NewsCardComponent: View {
     var article: NewsResponse.Article
     var body: some View {
         ZStack(alignment: .bottomLeading) {
+            KFImage(URL(string: article.urlToImage ?? ""))
+                .resizable()
+                .scaledToFill()
+                .frame(maxWidth: .infinity, maxHeight: 230)
             
+            LinearGradient(colors: [Color.clear, Color.black.opacity(0.7)],
+                           startPoint: .top, endPoint: .bottom)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(article.title)
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding(.horizontal, 16)
+            }
         }
         .padding(4)
         .frame(maxWidth: .infinity, maxHeight: 230, alignment: .leading)
