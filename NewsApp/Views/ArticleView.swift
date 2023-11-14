928a2c8e9ab10ccf3661a5f135c327b5961aa881
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-14 15:42:47 -0300

    creating articleview

---
+++ NewsApp/Views/ArticleView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleView.swift
+//  NewsApp
+//
+//  Created by Marcelo Amaral Alves on 2023-11-14.
+//
+
+import SwiftUI
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleView()
+}
