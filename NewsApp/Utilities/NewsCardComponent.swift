df21fc52b7ef0729a6951f26e9fe651e15bd9de6
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-15 16:46:25 -0300

    working on newscardcomponent

--- NewsApp/Utilities/NewsCardComponent.swift
+++ NewsApp/Utilities/NewsCardComponent.swift
@@ -10,6 +10,8 @@ import Kingfisher
 
 struct NewsCardComponent: View {
     var article: NewsResponse.Article
+    var type: String
+    
     var body: some View {
         ZStack(alignment: .bottomLeading) {
             KFImage(URL(string: article.urlToImage ?? ""))
@@ -20,29 +22,54 @@ struct NewsCardComponent: View {
             LinearGradient(colors: [Color.clear, Color.black.opacity(0.7)],
                            startPoint: .top, endPoint: .bottom)
             
-            VStack(alignment: .leading, spacing: 4) {
-                Text(article.title)
-                    .font(.title3)
-                    .foregroundColor(.white)
-                    .bold()
-                    .padding(.horizontal, 16)
+            VStack {
+                HStack {
+                    if type == "Positive" {
+                        Circle()
+                            .fill(.green)
+                            .frame(width: 8, height: 8)
+                            .padding(.leading, 8)
+                        Spacer()
+                    } else if type == "Negative" {
+                        Spacer()
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 8, height: 8)
+                            .padding(.leading)
+                    } else {
+                        Spacer()
+                        Circle()
+                            .fill(.gray)
+                            .frame(width: 8, height: 8)
+                        Spacer()
+                    }
+                }
                 
-                Text(article.description ?? "")
-                    .font(.caption2)
-                    .foregroundColor(.white)
-                    .bold()
-                    .lineLimit(1)
-                    .truncationMode(.tail)
-                    .padding(.bottom, 4)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(article.title)
+                        .font(.title3)
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(.horizontal, 16)
+                    
+                    Text(article.description ?? "")
+                        .font(.caption2)
+                        .foregroundColor(.white)
+                        .bold()
+                        .lineLimit(1)
+                        .truncationMode(.tail)
+                        .padding(.bottom, 4)
+                }
             }
+            .padding(4)
+            .frame(maxWidth: .infinity, maxHeight: 230, alignment: .leading)
+            .cornerRadius(8)
+            .shadow(radius: 4)
         }
-        .padding(4)
-        .frame(maxWidth: .infinity, maxHeight: 230, alignment: .leading)
-        .cornerRadius(8)
-        .shadow(radius: 4)
     }
 }
 
 #Preview {
-    NewsCardComponent(article: NewsResponse.Article(title: "", url: ""))
+    NewsCardComponent(article: NewsResponse.Article(title: "", url: ""), type: "Positive")
 }
