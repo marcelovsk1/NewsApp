6ce4a7783354437ad5c1c4736065fffb1150a3b2
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-15 16:41:13 -0300

    implementing machinelearn

--- NewsApp/Utilities/TextClassifier.swift
+++ NewsApp/Utilities/TextClassifier.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import CoreML
+
+func predictSentement(text: String) -> String? {
+    do {
+        let config = MLModelConfiguration()
+        let sentimentClassifier = try MyTextClassifier(configuration: config)
+        let prediction = try sentimentClassifier.prediction(text: text)
+        return prediction.label
+    } catch {
+        print("Failed to make prediction: \(error.localizedDescription)")
+        return nil
+    }
+}
