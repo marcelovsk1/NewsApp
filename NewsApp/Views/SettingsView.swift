9b00352ba49b6f765f8b2eca2390e737ff594793
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-16 05:05:58 -0300

    start to working on settingsview

--- NewsApp/Views/SettingsView.swift
+++ NewsApp/Views/SettingsView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SettingsView: View {
+    @StateObject var purchaseViewModel = PurchaseViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
