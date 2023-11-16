53efc9fcafed96df0a40fe998a0e4683afc28ca2
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-16 05:08:20 -0300

    implementing the buy func

--- NewsApp/Views/SettingsView.swift
+++ NewsApp/Views/SettingsView.swift
@@ -10,9 +10,20 @@ import StoreKit
 
 struct SettingsView: View {
     @StateObject var purchaseViewModel = PurchaseViewModel()
+    @AppStorage("purchased") var purchased = false
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    func buy(product: Product) async {
+        do {
+            if try await purchaseViewModel.purchase(product) != nil {
+                purchased = true
+            }
+        } catch {
+            print("Purchased failed")
+        }
     }
 }
 
